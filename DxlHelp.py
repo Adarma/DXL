da6c27175d77b2b0d91443a9d5ff889c2b90a816
Adam Cadamally <adarma@github.fake>
2013-06-11 11:15:54 +0200

    Initial commit

---
+++ DxlHelp.py
@@ -0,0 +1,31 @@
+import sublime
+import sublime_plugin
+import subprocess
+import os.path
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def OpenDxlHelp(text):
+    full_path = os.path.join(BASE_PATH, "keyhh.exe") + " -#klink " + text + " " + os.path.join(BASE_PATH, "dxl.chm")
+    subprocess.Popen(full_path)
+    sublime.status_message(text)
+
+
+class DxlKeywordHelpCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        curr_view = self.view
+        curr_sel = curr_view.sel()[0]
+        if curr_view.match_selector(curr_sel.begin(), 'source.dxl'):
+
+            word_end = curr_sel.end()
+            if curr_sel.empty():
+                word = curr_view.substr(curr_view.word(word_end)).lower()
+            else:
+                word = curr_view.substr(curr_sel).lower()
+            if word is None or len(word) <= 1:
+                sublime.status_message('No function selected')
+
+            OpenDxlHelp(word)
+        else:
+            sublime.status_message("No Help Available")
